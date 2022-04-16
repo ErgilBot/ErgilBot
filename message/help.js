e5deedda23b21614f16cb0f5d012af99a0c2c9b5
ErgilBot <ergilcrackd@gmail.comergilcrackd@gmail.comcleaecleargit config --global user.name ErgilBot/[6~[6~[6~~~git config --global user.email ergilcrackd@gmail.comclear>
2022-04-16 22:12:30 +0800

    ErgilBot

---
+++ message/help.js
@@ -0,0 +1,522 @@
+exports.OwnerMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *OwnerMenu* ⊰────
+│
+│➺ *${prefix}banned* _@tag_
+│➺ *${prefix}unban* _@tag_
+│➺ *${prefix}autoread*
+│➺ *${prefix}autovn*
+│➺ *${prefix}bc*
+│➺ *${prefix}bc2*
+│➺ *${prefix}bcgc*
+│➺ *${prefix}bcgc2*
+│➺ *${prefix}bcnowm*
+│➺ *${prefix}clearall*
+│➺ *${prefix}addrespon*
+│➺ *${prefix}setcmd*
+│➺ *${prefix}delrespon* [ code respon ] 
+│➺ *${prefix}delcmd* [ tag stick cmd ]
+│➺ *${prefix}premium* [ add/del ]
+│➺ *${prefix}join*
+│➺ *${prefix}self*
+│➺ *${prefix}public*
+│➺ *${prefix}shutdown*
+│➺ *${prefix}start*
+│➺ *${prefix}restart*
+│➺ *${prefix}leaveall* [ ban ]
+│➺ *${prefix}freelimit*
+│➺ *${prefix}term* [ code ]
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.AdminMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *AdminMenu* ⊰────
+│
+│➺ *${prefix}antilink*
+│➺ *${prefix}linkgc*
+│➺ *${prefix}kick*
+│➺ *${prefix}add*
+│➺ *${prefix}demote*
+│➺ *${prefix}promote*
+│➺ *${prefix}setgrupname*
+│➺ *${prefix}setdesc*
+│➺ *${prefix}setppgrup*
+│➺ *${prefix}tagall*
+│➺ *${prefix}kickall*
+│➺ *${prefix}leave* 
+│➺ *${prefix}listonline* 
+│➺ *${prefix}hidetag*
+│➺ *${prefix}sider*
+│➺ *${prefix}opentime*
+│➺ *${prefix}closetime*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.RpgMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *RpgMenu* ⊰────
+│
+│➺ *${prefix}judi [ beta ]
+│➺ *${prefix}buylimit*
+│➺ *${prefix}inventory*
+│➺ *${prefix}leaderboard*
+│➺ *${prefix}mancing*
+│➺ *${prefix}mining*
+│➺ *${prefix}adventure*
+│➺ *${prefix}basicgachakoin*
+│➺ *${prefix}basicgachaemas*
+│➺ *${prefix}basicgachabesi*
+│➺ *${prefix}basicgachadm* 
+│➺ *${prefix}basicgachaikan* 
+│➺ *${prefix}vipgachaikan*
+│➺ *${prefix}vipgachadm*
+│➺ *${prefix}vipgachabesi*
+│➺ *${prefix}vipgachaemas*
+│➺ *${prefix}vipgachakoin*
+│➺ *${prefix}sellikan*
+│➺ *${prefix}sellbesi*
+│➺ *${prefix}sellemas*
+│➺ *${prefix}selldiamond*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.DownloadMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *DownloadMenu* ⊰────
+│
+│➺ *${prefix}ytmp3* [ link yt ]
+│➺ *${prefix}ytmp4* [ link yt ]
+│➺ *${prefix}playmp3* [ judul yt ]
+│➺ *${prefix}playmp4* [ judul yt ]
+│➺ *${prefix}ttnowm* [ link tiktok ]
+│➺ *${prefix}ttwm* [ link tiktok ]
+│➺ *${prefix}tiktokmusic* [ link tiktok ]
+│➺ *${prefix}lirik* [ judul ]
+│➺ *${prefix}igdl* [ link ig ]
+│➺ *${prefix}twitterdl* [ link twitter ]
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.SearchMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *SearchMenu* ⊰────
+│
+│➺ *${prefix}herolist*
+│➺ *${prefix}herodetail* [ nama hero ]
+│➺ *${prefix}playstore* [ nama apk ]
+│➺ *${prefix}linkwa* [ nama gc ]
+│➺ *${prefix}pinterest* [ nama foto ]
+│➺ *${prefix}gimage* [ nama foto ]
+│➺ *${prefix}yandex* [ nama foto ]
+│➺ *${prefix}brainly* [ soal ]
+│➺ *${prefix}brainly2* [ reply foto soal ]
+│➺ *${prefix}wikipedia* [ query ]
+│➺ *${prefix}kbbi* [ query ]
+│➺ *${prefix}kodepos* [ kota ]
+│➺ *${prefix}bing* [ query ]
+│➺ *${prefix}mangatoon* [ query ]
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.ConvertMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *ConvertMenu* ⊰────
+│
+│➺ *${prefix}robot* [ reply audio ]
+│➺ *${prefix}gemuk* [ reply audio ]
+│➺ *${prefix}balik* [ reply audio ]
+│➺ *${prefix}bass* [ reply audio ]
+│➺ *${prefix}tourl*
+│➺ *${prefix}img2url*
+│➺ *${prefix}tovideo*
+│➺ *${prefix}tomp3*
+│➺ *${prefix}pmeme*
+│➺ *${prefix}smeme*
+│➺ *${prefix}take*
+│➺ *${prefix}sticker*
+│➺ *${prefix}sgif*
+│➺ *${prefix}toimg*
+│➺ *${prefix}togif*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.MakerMenu = (prefix, NamaBot) => {
+return`
+┌─────⊱ *MakerMenu* ⊰────
+│
+│➺ *${prefix}shadow* _Text_
+│➺ *${prefix}romantic* _Text_
+│➺ *${prefix}smoke* _Text_
+│➺ *${prefix}burnpaper* _Text_
+│➺ *${prefix}lovemessage* _Text_
+│➺ *${prefix}undergrass* _Text_
+│➺ *${prefix}doubleheart* _Text_
+│➺ *${prefix}coffecup* _Text_
+│➺ *${prefix}coffecup2* _Text_
+│➺ *${prefix}butterfly* _Text_
+│➺ *${prefix}cup* _Text_
+│➺ *${prefix}cup2* _Text_
+│➺ *${prefix}wolfmetal* _Text_
+│➺ *${prefix}underwater* _Text_
+│➺ *${prefix}roses* _Text_
+│➺ *${prefix}whitecube* _Text_
+│➺ *${prefix}nightsky* _Text_
+│➺ *${prefix}flaming* _Text_
+│➺ *${prefix}naruto* _Text_
+│➺ *${prefix}csgobanner* _Text_
+│➺ *${prefix}greenleaves* _Text_
+│➺ *${prefix}harrypotter* _Text_
+│➺ *${prefix}lolbanner* _Text_
+│➺ *${prefix}neondevil* _Text_
+│➺ *${prefix}3dshiny* _Text_
+│➺ *${prefix}zombie3d* _Text_
+│➺ *${prefix}wordgreen* _Text_
+│➺ *${prefix}cloud* _Text_
+│➺ *${prefix}metal* _Text_
+│➺ *${prefix}neon* _Text_
+│➺ *${prefix}graffiticollor* _Text_
+│➺ *${prefix}lightgalaxy* _Text_
+│➺ *${prefix}hotmetalic* _Text_
+│➺ *${prefix}snake* _Text_
+│➺ *${prefix}graffiti5* _Text_
+│➺ *${prefix}graffiti3* _Text_
+│➺ *${prefix}graffiti* _Text_
+│➺ *${prefix}neon2* _Text_
+│➺ *${prefix}thunder* _Text_
+│➺ *${prefix}startsnight* _Text_
+│➺ *${prefix}cake* _Text_
+│➺ *${prefix}writingchalk* _Text_
+│➺ *${prefix}birthdaycake* _Text_
+│➺ *${prefix}3dhologram* _Text_
+│➺ *${prefix}galaxystyle* _Text_
+│➺ *${prefix}lighteffects* _Text_
+│➺ *${prefix}greenbrush* _Text_
+│➺ *${prefix}cakes* _Text_
+│➺ *${prefix}startsnight2* _Text_
+│➺ *${prefix}glitch* _Text1|Text2_
+│➺ *${prefix}pubg* _Text1|Text2_
+│➺ *${prefix}8bit* _Text1|Text2_
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.IslamiMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *IslamiMenu* ⊰────
+│
+│➺ *${prefix}kisahnabi* [ nama nabi ]
+│➺ *${prefix}doaharian*
+│➺ *${prefix}niatshalat*
+│➺ *${prefix}tahlil*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.GameMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *FunMenu* ⊰────
+│
+│➺ *${prefix}fitnah* < @tag|pesan|balasan bot >
+│➺ *${prefix}fitnahpc* < 62xx|pesan|balasan bot >
+│➺ *${prefix}artinama* < nama >
+│➺ *${prefix}artimimpi* < mimpi mu >
+│➺ *${prefix}caklontong*
+│➺ *${prefix}tebakgambar*
+│➺ *${prefix}tictactoe* @tag lawan
+│➺ *${prefix}gelud* @tag lawan
+│➺ *${prefix}delsesittt*
+│➺ *${prefix}delsesigelud*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.SoundMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *SoundMenu* ⊰────
+│
+│➺ *${prefix}sound1*
+│➺ *${prefix}sound2*
+│➺ *${prefix}sound3*
+│➺ *${prefix}sound4*
+│➺ *${prefix}sound5*
+│➺ *${prefix}sound6*
+│➺ *${prefix}sound7*
+│➺ *${prefix}sound8*
+│➺ *${prefix}sound9*
+│➺ *${prefix}sound10*
+│➺ *${prefix}sound11*
+│➺ *${prefix}sound12*
+│➺ *${prefix}sound13*
+│➺ *${prefix}sound14*
+│➺ *${prefix}sound15*
+│➺ *${prefix}sound16*
+│➺ *${prefix}sound17*
+│➺ *${prefix}sound18*
+│➺ *${prefix}sound19*
+│➺ *${prefix}sound20*
+│➺ *${prefix}sound21*
+│➺ *${prefix}sound22*
+│➺ *${prefix}sound23*
+│➺ *${prefix}sound24*
+│➺ *${prefix}sound25*
+│➺ *${prefix}sound26*
+│➺ *${prefix}sound27*
+│➺ *${prefix}sound28*
+│➺ *${prefix}sound29*
+│➺ *${prefix}sound30*
+│➺ *${prefix}sound31*
+│➺ *${prefix}sound32*
+│➺ *${prefix}sound33*
+│➺ *${prefix}sound34*
+│➺ *${prefix}sound35*
+│➺ *${prefix}sound36*
+│➺ *${prefix}sound37*
+│➺ *${prefix}sound38*
+│➺ *${prefix}sound39*
+│➺ *${prefix}sound40*
+│➺ *${prefix}sound41*
+│➺ *${prefix}sound42*
+│➺ *${prefix}sound43*
+│➺ *${prefix}sound44*
+│➺ *${prefix}sound45*
+│➺ *${prefix}sound46*
+│➺ *${prefix}sound47*
+│➺ *${prefix}sound48*
+│➺ *${prefix}sound49*
+│➺ *${prefix}sound50*
+│➺ *${prefix}sound51*
+│➺ *${prefix}sound52*
+│➺ *${prefix}sound53*
+│➺ *${prefix}sound54*
+│➺ *${prefix}sound56*
+│➺ *${prefix}sound57*
+│➺ *${prefix}sound58*
+│➺ *${prefix}sound59*
+│➺ *${prefix}sound60*
+│➺ *${prefix}sound61*
+│➺ *${prefix}sound62*
+│➺ *${prefix}sound63*
+│➺ *${prefix}sound64*
+│➺ *${prefix}sound65*
+│➺ *${prefix}sound66*
+│➺ *${prefix}sound67*
+│➺ *${prefix}sound68*
+│➺ *${prefix}sound69*
+│➺ *${prefix}sound70*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+exports.StorageMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *StorageMenu* ⊰────
+│
+│➺ *${prefix}addvn*
+│➺ *${prefix}addfoto*
+│➺ *${prefix}addsticker*
+│➺ *${prefix}delvn*
+│➺ *${prefix}delfoto*
+│➺ *${prefix}delsticker*
+│➺ *${prefix}listvn*
+│➺ *${prefix}listfoto*
+│➺ *${prefix}liststicker*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.GroupMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *GroupMenu* ⊰────
+│
+│➺ *${prefix}ganteng
+│➺ *${prefix}cantik
+│➺ *${prefix}jelek
+│➺ *${prefix}goblok
+│➺ *${prefix}bego
+│➺ *${prefix}pinter
+│➺ *${prefix}jago
+│➺ *${prefix}babi
+│➺ *${prefix}beban
+│➺ *${prefix}baik
+│➺ *${prefix}jahat
+│➺ *${prefix}anjing
+│➺ *${prefix}monyet
+│➺ *${prefix}haram
+│➺ *${prefix}kontol
+│➺ *${prefix}pakboy
+│➺ *${prefix}pakgirl
+│➺ *${prefix}sadboy
+│➺ *${prefix}sadgirl
+│➺ *${prefix}wibu
+│➺ *${prefix}nolep
+│➺ *${prefix}hebat
+│➺ *${prefix}vsadboy
+│➺ *${prefix}vpakboy
+│➺ *${prefix}vbaik
+│➺ *${prefix}vjago
+│➺ *${prefix}vjelek
+│➺ *${prefix}vcantik
+│➺ *${prefix}vpinter
+│➺ *${prefix}vbeban
+│➺ *${prefix}vkontol
+│➺ *${prefix}vhebat
+│➺ *${prefix}vwibu
+│➺ *${prefix}vharam
+│➺ *${prefix}vbabi
+│➺ *${prefix}vbego
+│➺ *${prefix}vganteng
+│➺ *${prefix}vanjing
+│➺ *${prefix}vmonyet
+│➺ *${prefix}vsadgirl
+│➺ *${prefix}vpakgirl
+│➺ *${prefix}vjahat
+│➺ *${prefix}vnolep
+│➺ *${prefix}vgoblok
+│➺ *${prefix}xsadboy <@tag>
+│➺ *${prefix}xpakboy <@tag>
+│➺ *${prefix}xbaik <@tag>
+│➺ *${prefix}xjago <@tag>
+│➺ *${prefix}xjelek <@tag>
+│➺ *${prefix}xcantik <@tag>
+│➺ *${prefix}xpinter <@tag>
+│➺ *${prefix}xbeban <@tag>
+│➺ *${prefix}xkontol <@tag>
+│➺ *${prefix}xhebat <@tag>
+│➺ *${prefix}xwibu <@tag>
+│➺ *${prefix}xharam <@tag>
+│➺ *${prefix}xbabi <@tag>
+│➺ *${prefix}xbego <@tag>
+│➺ *${prefix}xganteng <@tag>
+│➺ *${prefix}xanjing <@tag>
+│➺ *${prefix}xmonyet <@tag>
+│➺ *${prefix}xsadgirl <@tag>
+│➺ *${prefix}xpakgirl <@tag>
+│➺ *${prefix}xjahat <@tag>
+│➺ *${prefix}xnolep <@tag>
+│➺ *${prefix}xgoblok <@tag>
+│➺ *${prefix}zsadboy*
+│➺ *${prefix}zpakboy*
+│➺ *${prefix}zbaik*
+│➺ *${prefix}zjago*
+│➺ *${prefix}zjelek*
+│➺ *${prefix}zcantik*
+│➺ *${prefix}zpinter*
+│➺ *${prefix}zbeban*
+│➺ *${prefix}zkontol*
+│➺ *${prefix}zhebat*
+│➺ *${prefix}zwibu*
+│➺ *${prefix}zharam*
+│➺ *${prefix}zbabi*
+│➺ *${prefix}zbego*
+│➺ *${prefix}zganteng*
+│➺ *${prefix}zanjing*
+│➺ *${prefix}zmonyet*
+│➺ *${prefix}zsadgirl*
+│➺ *${prefix}zpakgirl*
+│➺ *${prefix}zjahat*
+│➺ *${prefix}znolep*
+│➺ *${prefix}zgoblok*
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.CekMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *CekMenu* ⊰────
+│
+│➺ *${prefix}gantengcek
+│➺ *${prefix}cantikcek
+│➺ *${prefix}jelekcek
+│➺ *${prefix}goblokcek
+│➺ *${prefix}begocek
+│➺ *${prefix}pintercek
+│➺ *${prefix}jagocek
+│➺ *${prefix}nolepcek
+│➺ *${prefix}babicek
+│➺ *${prefix}bebancek
+│➺ *${prefix}baikcek
+│➺ *${prefix}jahatcek
+│➺ *${prefix}anjingcek
+│➺ *${prefix}haramcek
+│➺ *${prefix}kontolcek
+│➺ *${prefix}pakboycek
+│➺ *${prefix}pakgirlcek
+│➺ *${prefix}sangecek
+│➺ *${prefix}bapercek
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.OtherMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *OtherMenu* ⊰────
+│
+│➺ *${prefix}getbio* < @tag > 
+│➺ *${prefix}getdesk*
+│➺ *${prefix}getpic* < @tag >
+│➺ *${prefix}donasi*
+│➺ *${prefix}script*
+│➺ *${prefix}runtime*
+│➺ *${prefix}premiumcheck*
+│➺ *${prefix}listpremium*
+│➺ *${prefix}listrespon*
+│➺ *${prefix}listgc*
+│➺ *${prefix}speed*
+│➺ *${prefix}afk* < alasan >
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
+
+exports.CanvasMenu = (prefix, NamaBot) =>  {
+return`
+┌─────⊱ *CanvasMenu* ⊰────
+│
+│➺ *${prefix}hacker* _ReplyFoto_ 
+│➺ *${prefix}hacker2* _ReplyFoto_ 
+│➺ *${prefix}gay* _ReplyFoto_ 
+│➺ *${prefix}greyscale* _ReplyFoto_ 
+│➺ *${prefix}invert* _ReplyFoto_ 
+│➺ *${prefix}wanted* _ReplyFoto_ 
+│➺ *${prefix}sepia* _ReplyFoto_ 
+│➺ *${prefix}triggered* _ReplyFoto_ 
+│➺ *${prefix}crush* _ReplyFoto_ 
+│➺ *${prefix}approved* _ReplyFoto_ 
+│➺ *${prefix}airpods* _ReplyFoto_ 
+│➺ *${prefix}meth* _ReplyFoto_ 
+│➺ *${prefix}communism* _ReplyFoto_ 
+│➺ *${prefix}bjp* _ReplyFoto_ 
+│➺ *${prefix}captcha* _ReplyFoto_ 
+│➺ *${prefix}contiuned* _ReplyFoto_ 
+│➺ *${prefix}police* _ReplyFoto_ 
+│➺ *${prefix}linus* _ReplyFoto_ 
+│➺ *${prefix}what* _ReplyFoto_ 
+│➺ *${prefix}ad* _ReplyFoto_ 
+│➺ *${prefix}bobross* _ReplyFoto_ 
+│➺ *${prefix}confusedstonk* _ReplyFoto_ 
+│➺ *${prefix}delete* _ReplyFoto_ 
+│➺ *${prefix}discordblack* _ReplyFoto_ 
+│➺ *${prefix}discordblue* _ReplyFoto_ 
+│➺ *${prefix}facepalm* _ReplyFoto_ 
+│➺ *${prefix}hitler* _ReplyFoto_ 
+│➺ *${prefix}jail* _ReplyFoto_ 
+│➺ *${prefix}karaba* _ReplyFoto_ 
+│➺ *${prefix}mms* _ReplyFoto_ 
+│➺ *${prefix}stonk* _ReplyFoto_ 
+│➺ *${prefix}notstonk* _ReplyFoto_ 
+│➺ *${prefix}poutine* _ReplyFoto_ 
+│➺ *${prefix}rip* _ReplyFoto_ 
+│➺ *${prefix}tatoo* _ReplyFoto_ 
+│➺ *${prefix}thomas* _ReplyFoto_ 
+│➺ *${prefix}trash* _ReplyFoto_ 
+│➺ *${prefix}circle* _ReplyFoto_ 
+│➺ *${prefix}bill* _ReplyFoto_ 
+│➺ *${prefix}udf* _ReplyFoto_
+│
+└─────⊱ *${NamaBot}* ⊰────
+`}
